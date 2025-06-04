4293e99ba73a67ce3909432878c4a8c6b15bd5b0
Ekaterina Grishina <grischina.kotya17@gmail.com>
2025-06-04 17:47:24 +0200

    Add globalStyle and types

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,26 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+    export interface DefaultTheme {
+        colors: {
+            surface: string,
+            default: string,
+            muted: string,
+            surface5: string,
+            surface3: string,
+            disabled: string,
+            disabledbtn: string,
+            blue100: string,
+            blue060: string,
+            blue050: string,
+            red: string,
+            red050: string,
+            violet: string,
+        },
+        device: {
+            mobile: string,
+            tablet: string,
+            dasktop: string,
+        },
+    }
+}
