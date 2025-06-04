b149baf54c9816d789cd480124867a891f6ed65f
Ekaterina Grishina <grischina.kotya17@gmail.com>
2025-06-04 16:51:41 +0200

    Update App with basename and hide other pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import WelcomePage from "./pages/WelcomePage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/ai-school-kid">
       <Routes>
         <Route path="/" element={<WelcomePage />} />
-        <Route path="/upload" element={<UploadImagesPage />} />
+        {/* <Route path="/upload" element={<UploadImagesPage />} />
         <Route path="/questions" element={<QuestionsPage />} />
-        <Route path="/result" element={<ResultPage />} />
+        <Route path="/result" element={<ResultPage />} /> */}
       </Routes>
     </BrowserRouter>
   )
