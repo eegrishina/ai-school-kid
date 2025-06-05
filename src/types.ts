6ffec8015a55c9f42719ec919014ef6aa39e7e78
Ekaterina Grishina <grischina.kotya17@gmail.com>
2025-06-05 13:04:45 +0200

    Add types for child info

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,15 @@ export interface PhotosState {
     photos: UploadedPhoto[];
     taskId: string | null;
 }
+
+export interface ChildInfo {
+    childName: string;
+    childDOB: string;
+    childGender: string;
+    parentName: string;
+}
+
+export interface ChildInfoFormState {
+    childInfo: ChildInfo;
+    isChildInfoValid: boolean;
+}
